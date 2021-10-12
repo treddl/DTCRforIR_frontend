52c602299c5fd5be83594532b3f8c6e985178fad
treddl <73252042+treddl@users.noreply.github.com>
2021-10-12 16:24:20 +0200

    updated nodejs install method

--- setup_frontend.sh
+++ setup_frontend.sh
@@ -1,4 +1,7 @@
-sudo apt install nodejs && \
-npm install -g npm@latest && \
+sudo apt remove nodejs && \
+sudo apt purge nodejs && \
+sudo apt autoremove  && \
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -  && \
+sudo apt-get install -y nodejs && \
 npm install && \
 cd ..
