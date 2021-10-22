65daf4d432b1707a3e31fd853dd3433923f45347
treddl <73252042+treddl@users.noreply.github.com>
2021-10-22 13:51:37 +0200

    remove credentials

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/firestore'
 
 // firebase init
 const firebaseConfig = {
-    apiKey: "AIzaSyAZ5kdu227j0r7IOqNNE0kET5spK3fl_S4",
-    authDomain: "digitaltwincyberrange.firebaseapp.com",
-    projectId: "digitaltwincyberrange",
-    storageBucket: "digitaltwincyberrange.appspot.com",
-    messagingSenderId: "338562189196",
-    appId: "1:338562189196:web:92cc478f2ff79cac1b78a9"
+// add your config here
 
 }
 firebase.initializeApp(firebaseConfig)
